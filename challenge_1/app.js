ab17aff67fab3a03f82dfbbbeef4cbe61597c01e
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-09 12:11:59 -0700

    Fix logic for validating winners

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,11 +1,15 @@
-var strokes = { X: 1, O: 2 };
+var strokes = { X: 0, O: 1 };
 
 var board = [];
+
+var active;
+
+var winner;
 
 var preparePage = (boardSize, cellSize) => {
   var appNode = document.getElementById("app");
   appNode.appendChild(drawBoard(boardSize, cellSize));
-  prepareBoard(boardSize);
+  prepareGame(boardSize);
 };
 
 var drawBoard = (boardSize, cellSize) => {
@@ -18,33 +22,36 @@ var drawBoard = (boardSize, cellSize) => {
       cell.setAttribute("class", "cell");
       cell.setAttribute("row", i);
       cell.setAttribute("col", j);
+      cell.style.border = "1px solid";
+      cell.style.cursor = "pointer";
       cell.style.height = cell.style.width = `${cellSize}px`;
-      cell.onclick = e => handleCellClick(e);
+      cell.onclick = e => handleCellClick(e.target);
     }
   }
   return boardNode;
 };
 
-var prepareBoard = boardSize => {
-  board = [];
-  for (var i = 0; i < boardSize; i++) {
-    board.push([]);
-    for (var j = 0; j < boardSize; j++) {
-      board[i].push(0);
-    }
-  }
+var prepareGame = boardSize => {
+  board = new2DArray(boardSize);
   var cells = document.getElementsByClassName("cell");
   for (var i = 0; i < cells.length; i++) {
     cells[i].innerHTML = "";
   }
   currentType = "X";
+  active = true;
 };
-preparePage(3, 16);
 
-var handleCellClick = e => {
-  var cell = e.target;
-  cell.innerHTML = currentType;
-  setStroke(cell.getAttribute("row"), cell.getAttribute("col"));
+var handleCellClick = cell => {
+  if (active && !cell.innerHTML) {
+    cell.innerHTML = currentType;
+    winningMove = setStroke(cell.getAttribute("row"), cell.getAttribute("col"));
+    if (winningMove) {
+      active = false;
+      winner = currentType;
+    } else {
+      currentType = currentType === "X" ? "O" : "X";
+    }
+  }
 };
 
 var setStroke = (row, col) => {
@@ -56,26 +63,46 @@ var setStroke = (row, col) => {
 
 var checkWinner = (row, col, type) => {
   var strokeVal = strokes[type];
-  var rowWinner = rowSum(row) === board.length * strokeVal;
-  var colWinner = colSum(col) === board.length * strokeVal;
-  var diagonalWinner =
-    majDiagonalSum() === board.length * strokeVal ||
-    minDiagonalSum() === board.length * strokeVal;
-  return rowWinner || colWinner || diagonalWinner;
+  return (
+    rowWinner(row, strokeVal) ||
+    colWinner(col, strokeVal) ||
+    majDiagonalWinner(strokeVal) ||
+    minDiagonalWinner(strokeVal)
+  );
 };
 
-var rowSum = row => board[row].reduce((sum, current) => sum + current, 0);
+var rowWinner = (row, strokeVal) =>
+  board[row].reduce((isWinning, cell) => cell === strokeVal && isWinning, true);
 
-var colSum = col => board.reduce((sum, row) => sum + row[col], 0);
+var colWinner = (col, strokeVal) =>
+  board.reduce((isWinning, row) => row[col] === strokeVal && isWinning, true);
 
-var majDiagonalSum = () => {
-  var sum = 0;
-  board.forEach((row, index) => (sum += row[index]));
-  return sum;
+var majDiagonalWinner = strokeVal => {
+  isWinning = true;
+  board.forEach(
+    (row, index) => (isWinning = row[index] === strokeVal && isWinning)
+  );
+  return isWinning;
 };
 
-var minDiagonalSum = () => {
-  var sum = 0;
-  board.forEach((row, index) => (sum += row[board.length - index - 1]));
-  return sum;
+var minDiagonalWinner = strokeVal => {
+  isWinning = true;
+  board.forEach(
+    (row, index) =>
+      (isWinning = row[board.length - index - 1] === strokeVal && isWinning)
+  );
+  return isWinning;
 };
+
+var new2DArray = size => {
+  var arr = [];
+  for (var i = 0; i < size; i++) {
+    arr.push([]);
+    for (var j = 0; j < size; j++) {
+      arr[i].push(null);
+    }
+  }
+  return arr;
+};
+
+preparePage(3, 16);

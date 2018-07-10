7ce482335b781a6926667bcc852ec3811e92a8a8
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-10 11:19:43 -0700

    Set up basic client and server files

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,24 @@
+"use strict";
 
+$(document).ready(() => {
+  const endpoint = "/json";
+  const $jsonText = $("#json-text");
 
+  $("#json-parser").on("submit", e => {
+    processJSON($jsonText.val());
+    e.preventDefault();
+  });
+
+  const processJSON = json => {
+    $.ajax({
+      url: endpoint,
+      type: "POST",
+      data: json,
+      success: function(data) {
+        console.log(data);
+        $jsonText.val("");
+      },
+      error: function(err) {}
+    });
+  };
+});

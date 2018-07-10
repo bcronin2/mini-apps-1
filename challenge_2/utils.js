e7196935fd92b3f75fbf7ee9df2b45d7b49d0e4b
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-10 11:20:08 -0700

    Get basic parser functionality working

---
+++ challenge_2/utils.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const parseToCSV = jsonString => {
+  let jsonData = JSON.parse(jsonString);
+  let csv = getHeading(jsonData).join(",") + "\n";
+  csv += getRows(jsonData);
+  return csv;
+};
+
+const getHeading = jsonData => {
+  while (Array.isArray(jsonData)) {
+    jsonData = jsonData[0];
+  }
+  return Object.keys(jsonData).filter(key => key !== "children");
+};
+
+const getRows = jsonData => {
+  let flatJSON = flattenJSON(jsonData);
+  let rows = [];
+
+  flatJSON.forEach(obj => rows.push(getRow(obj)));
+
+  return rows.join("\n");
+};
+
+const getRow = jsonObj => {
+  let row = [];
+  for (let key in jsonObj) {
+    if (key !== "children") {
+      row.push(jsonObj[key]);
+    }
+  }
+  return row.join(",");
+};
+
+const flattenJSON = jsonData => {
+  var flatJSON = [];
+  if (Array.isArray(jsonData)) {
+    jsonData.forEach(
+      jsonDatum => (flatJSON = flatJSON.concat(flattenJSON(jsonDatum)))
+    );
+  } else {
+    let obj = {};
+    for (let key in jsonData) {
+      if (key !== "children") {
+        obj[key] = jsonData[key];
+      } else {
+        flatJSON = flatJSON.concat(flattenJSON(jsonData[key]));
+      }
+    }
+    flatJSON.push(obj);
+  }
+  return flatJSON;
+};
+
+module.exports = {
+  parseToCSV: parseToCSV
+};

816b28da789d1a304788f32f7411916cfaf35c32
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-11 11:29:52 -0700

    Set up basic npm config and server file

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+const express = require("express");
+const parser = require("body-parser");
+const port = process.env.PORT || 1024;
 
+const server = express();
 
+server.use(express.static(`${__dirname}/client`));
+server.use(parser.json());
+
+server.listen(port, () => console.log(`Listening on port ${port}...`));

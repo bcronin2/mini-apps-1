698f22a4e508b3987db023b800ac9e79effbb4aa
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-11 17:24:26 -0700

    Create basic form ppages/inputs and connect them with React component data

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,31 +2,153 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userId: 0,
-      userData: {},
-      currentData: {},
-      currentForm: "F1"
+      id: 0,
+      step: 0,
+      new: true,
+      stepData: {},
+      formData: new Array(4)
     };
   }
 
+  componentDidUpdate() {
+    let inputs = document.getElementsByTagName("input");
+    if (inputs.length) {
+      for (let i = 0; i < inputs.length; i++) {
+        let prefillData = this.state.formData[this.state.step];
+        inputs[i].value =
+          prefillData && prefillData[inputs[i].id]
+            ? prefillData[inputs[i].id]
+            : "";
+      }
+      inputs[0].focus();
+    }
+  }
+
+  handleChange(fieldId) {
+    this.state.stepData[fieldId] = document.getElementById(fieldId).value;
+  }
+
+  logIn() {}
+
   nextScreen() {
-    // send current data to server
-    // add current data to userData
-    // change currentForm
+    if (this.state.step > 0) {
+      this.state.formData[this.state.step] = this.state.stepData;
+      this.state.stepData = this.state.formData[this.state.step + 1] || {};
+    }
+    this.setState({ step: this.state.step + 1 });
   }
 
   prevScreen() {
-    //change currentForm
+    this.state.stepData = this.state.formData[this.state.step - 1] || {};
+    this.setState({ step: this.state.step - 1 });
   }
 
   checkout() {
-    // send data to server
-    // refresh state
+    // this.postData(this.state.data);
+    this.nextScreen();
   }
 
   render() {
-    return <div>Hellooo, world!</div>;
+    let content;
+    let next = <button onClick={this.nextScreen.bind(this)}>Next</button>;
+    let prev = <button onClick={this.prevScreen.bind(this)}>Next</button>;
+    if (this.state.step === 0) {
+      return (
+        <div className="content">
+          <div className="title">Welcome!!!</div>
+          <div className="body">Select what you'd like to purchase...</div>
+          <div className="nav">
+            <button onClick={this.nextScreen.bind(this)}>Continue</button>
+          </div>
+        </div>
+      );
+    } else if (this.state.step === 1) {
+      return (
+        <div className="content">
+          <div className="title">Step 1: Sign up</div>
+          <div className="body">
+            {this.inputElement(
+              "username",
+              "Enter a username...",
+              this.state.stepData
+            )}
+            {this.inputElement("email", "Enter your email...")}
+            {this.inputElement("password", "Enter a password...", "password")}
+          </div>
+          <div className="nav">
+            <button onClick={this.prevScreen.bind(this)}>Nevermind.</button>
+            <button onClick={this.nextScreen.bind(this)}>Continue</button>
+          </div>
+        </div>
+      );
+    } else if (this.state.step === 2) {
+      return (
+        <div className="content">
+          <div className="title">Step 2: Enter address</div>
+          <div className="body">
+            {this.inputElement("name", "Recipient's full name")}
+            {this.inputElement("street_1", "Street address 1")}
+            {this.inputElement("street_2", "Street address 2 (optional)")}
+            {this.inputElement("city", "City")}
+            {this.inputElement("state", "State")}
+            {this.inputElement("zip", "Zip code")}
+          </div>
+          <div className="nav">
+            <button onClick={this.prevScreen.bind(this)}>Go back</button>
+            <button onClick={this.nextScreen.bind(this)}>Continue</button>
+          </div>
+        </div>
+      );
+    } else if (this.state.step === 3) {
+      return (
+        <div className="content">
+          <div className="title">Step 3: Enter payment info</div>
+          <div className="body">
+            {this.inputElement("holder_name", "Cardholder's full name")}
+            {this.inputElement("card_number", "Card number")}
+            {this.inputElement("ccv_number", "CCV")}
+            {this.inputElement("expiration", "exp date")}
+          </div>
+          <div className="nav">
+            <button onClick={this.prevScreen.bind(this)}>Go back</button>
+            <button onClick={this.checkout.bind(this)}>Purchase!</button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="content">
+          <div className="title">Thanks for your purchase!</div>
+          <div className="body">You just bought some stuff.</div>
+          <div className="nav">
+            <button onClick={() => this.setState({ step: 0 })}>
+              Go back home
+            </button>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  inputElement(id, placeholder, type) {
+    return (
+      <input
+        id={id}
+        // value={this.state.stepData ? this.state.stepData[id] : ""}
+        placeholder={placeholder}
+        onChange={() => this.handleChange(id)}
+        type={type ? type : "text"}
+      />
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("dialog"));
+App.defaultProps = {
+  stepNames: {
+    1: "accounts",
+    2: "addresses",
+    3: "cards"
+  }
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));

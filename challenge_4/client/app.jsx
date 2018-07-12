2d86a00a5e1d87315421658710631fd54648d1b8
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-12 12:02:17 -0700

    Create basic page layout

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,2 +1,26 @@
+const React = require("react");
+// import { MovieList } from "./MovieList.jsx";
+// import { Search } from "./Search.jsx";
+// import { Filter } from "./Filter.jsx";
+// import { FindAndAdd } from "./FindAndAdd.jsx";
+// import IMDB from "../lib/IMDB.js";
+// import movieCollection from "../lib/movieCollection.js";
 
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div class="banner">
+          <div>Connect N</div>
+        </div>
+        <div class="content">
+          <div class="body">Content goes here</div>
+        </div>
+      </div>
+    );
+  }
+}
 
+App.defaultProps = {};
+
+module.exports = App;
--- challenge_4/client/main.js
+++ challenge_4/client/main.js
@@ -1,2 +1,7 @@
+import "./styles/styles.css";
+
 const React = require("react");
 const ReactDOM = require("react-dom");
+const App = require("./app.jsx");
+
+ReactDOM.render(<App />, document.getElementById("app"));

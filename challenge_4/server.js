41a90c8f232f9a5bab8720982d4802a6863fac92
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-12 19:26:59 -0700

    Enable posting of data to db

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,6 +1,10 @@
+"use strict";
+
 const express = require("express");
 const parser = require("body-parser");
 const port = process.env.PORT || 4444;
+
+const db = require("./db.js");
 
 const server = express();
 
@@ -8,3 +12,15 @@ server.use(express.static(`${__dirname}/public`));
 server.use(parser.json());
 
 server.listen(port, console.log(`Listening on port ${port}...`));
+
+server.post("/games", (req, res) => {
+  let json = req.body;
+  db.insertOne(json);
+  res.send(json);
+});
+
+server.get("/games", (req, res) => {
+  let results = db.fetchAll(results => {
+    res.send(results);
+  });
+});

d2118f71826aada4858499fc6350d9c1116f63d7
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-12 18:50:02 -0700

    Set up express server

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,10 @@
+const express = require("express");
+const parser = require("body-parser");
+const port = process.env.PORT || 4444;
 
+const server = express();
 
+server.use(express.static(`${__dirname}/public`));
+server.use(parser.json());
+
+server.listen(port, console.log(`Listening on port ${port}...`));
